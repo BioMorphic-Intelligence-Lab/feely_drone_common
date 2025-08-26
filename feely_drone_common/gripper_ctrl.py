0f22b05421f483aa378dd7dab00ab6268266fdf2
antbre <a.bredenbeck@tudelft.nl>
2025-08-26 15:00:37 +0200

    Make gripper_ctrl a pd behavior in sim

--- feely_drone_common/gripper_ctrl.py
+++ feely_drone_common/gripper_ctrl.py
@@ -2,16 +2,31 @@ import numpy as np
 
 class GripperCtrl:
 
-    def __init__(self, tau_max):
+    def __init__(self, tau_max, p_gain=0.01, d_gain=0.1):
         
         # Remember maximum force
         self.tau_max = tau_max
 
-    def reset(self):
-        pass
-
-    def open_to(self, alpha):
+        # Current alpha
+        self.alpha = 0.0
+        self.dalpha = 0.0
         
-        assert (0 <= alpha).all() and (alpha <= 1).all(), f"Commanded opening state ({alpha}) outside allowed range [0, 1]"
+        # Gains
+        self.p_gain = p_gain
+        self.d_gain = d_gain
 
-        return alpha * self.tau_max
+    def reset(self):
+        # Current alpha
+        self.alpha = 0.0
+        self.dalpha = 0.0
+
+    def open_to(self, alpha_des):
+        
+        assert (0 <= alpha_des).all() and (alpha_des <= 1).all(), f"Commanded opening state ({alpha_des}) outside allowed range [0, 1]"
+
+        err = (alpha_des - self.alpha)
+        alpha_new = np.clip(self.alpha + self.p_gain * err - self.d_gain * self.dalpha, 0, 1) 
+        self.dalpha = (alpha_new - self.alpha)
+        self.alpha = alpha_new
+
+        return  self.alpha*self.tau_max
