0f053a5f9599e726ab11b3aeb6c10c30c74fa729
antbre <a.bredenbeck@tudelft.nl>
2026-03-18 13:21:37 +0100

    Add hbars to assets

---
+++ feely_drone_common/assets/cylinders/__init__.py
@@ -0,0 +1,2 @@
+"""Generated cylinder URDF assets."""
+
--- feely_drone_common/utility.py
+++ feely_drone_common/utility.py
@@ -4,6 +4,9 @@ def get_urdf_path(name: str) -> str:
     if "cylinder" in name:
         with importlib.resources.path("feely_drone_common.assets.cylinders", name) as path:
             return str(path)
+    elif "hbar" in name:
+        with importlib.resources.path("feely_drone_common.assets.hbars", name) as path:
+            return str(path)
     else:
         with importlib.resources.path("feely_drone_common.assets", name) as path:
             return str(path)
